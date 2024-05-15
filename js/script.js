a8fa9db6855249bf20858b7b3cb27e0699d53cda
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-15 18:44:46 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2O-Unit-5-08/sw.js", {
+    scope: "/ICD2O-Unit-5-08/",
+  })
+}
+
+
+function myButtonClicked() {
+
+  // input
+  let numberA = parseInt(document.getElementById("number-a").value)
+  const numberB = parseInt(document.getElementById("number-b").value)
+  let counter = 0
+  let remainder = 0
+
+  while (true) {
+    if (numberA < numberB) {
+      remainder += numberA
+      break
+    } else {
+      document.getElementById("answer").innerHTML += numberA + " - " + numberB + " = " + (numberA - numberB) + "<br />"
+    }
+    numberA -= Math.abs(numberB)
+    counter++
+    console.log(numberA)
+  }
+  console.log(numberA)
+  document.getElementById("answer").innerHTML += "The answer is = " + counter + " with a remainder of " + remainder
+}
